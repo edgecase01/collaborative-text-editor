febe4e6362b6682ba9a01d5f2b2377f82a32b21b
edgecase01 <prathamvarma178@gmail.com>
2026-09-04 10:23:53 +0530

    implement cors for origin http://localhost:5173

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,13 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
 const PORT = 3000;
+
+app.use(cors({
+  origin: "http://localhost:5173"
+}));
 
 app.use(express.json());
 
